c7a5cb6364ffedcd222c578f3cbd07df584a8ff8
krishna221 <krishna.prasath.god@gmail.com>
2013-09-30 20:53:23 +0200

    Update meanFinder.R

--- R/meanFinder.R
+++ R/meanFinder.R
@@ -2,7 +2,7 @@
         if(distribution == "")
         {
             distribution = rnorm(50, mean=1, sd=1.5)
-            
-            message = mean(distribution)
         }
+        
+         message = mean(distribution)
     }
