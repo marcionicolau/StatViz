d993750fd515374dbb7bd323dc655d7babc6a1f4
krishna221 <krishna.prasath.god@gmail.com>
2013-09-30 17:52:20 +0200

    Update mean.R

--- R/mean.R
+++ R/mean.R
@@ -1,3 +1,8 @@
-    mean <- function(distribution = rnorm(50, mean=1, sd=1.5)) {
-        output = mean(distribution)
+    mean <- function(distribution = "") {
+        if(distribution == "")
+        {
+            distribution = rnorm(50, mean=1, sd=1.5)
+            
+            message = mean(distribution)
+        }
     }
