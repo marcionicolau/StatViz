e45eef62906700d1267d0f52afe7b5a3ea682a36
krishna221 <krishna.prasath.god@gmail.com>
2013-10-01 15:54:23 +0200

    Create getMedian.R

---
+++ R/getMedian.R
@@ -0,0 +1,17 @@
+    getMedian <- function(dataset = "",columnName = "") 
+    {
+        if(dataset == "")
+        {   
+            dataset = "beaver1";
+            columnName = "time";
+        }
+        else if(columnName == "")
+        {
+            # Load the first column name by default
+            columnName = names(eval(parse(text = dataset)))[1]; 
+        }
+        
+        distribution = eval(parse(text = paste(dataset,"$",columnName)));
+        
+        list(median = median(distribution));
+    }
