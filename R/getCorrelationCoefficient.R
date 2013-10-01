572d34134a3ed63022b1364b413653fdafc9ef78
krishna221 <krishna.prasath.god@gmail.com>
2013-10-01 15:55:08 +0200

    Create getCorrelationCoefficient.R

---
+++ R/getCorrelationCoefficient.R
@@ -0,0 +1,25 @@
+    getCorrelationCoefficient <- function(dataset = "",columnNameX = "", columnNameY = "", method = "") 
+    {
+        if(dataset == "")
+        {   
+            dataset = "beaver1";
+            columnNameX = "time";
+            columnNameY = "temp";
+        }
+        else if(columnNameX == "" || columnNameY == "")
+        {
+            # Load the first column name by default
+            columnNameX = names(eval(parse(text = dataset)))[1]; 
+            columnNameY = names(eval(parse(text = dataset)))[2]; 
+        }
+        
+        distributionX = eval(parse(text = paste(dataset,"$",columnNameX)));
+        distributionY = eval(parse(text = paste(dataset,"$",columnNameY)));
+        
+        if(method == "")
+        {
+            method = "pearson";
+        }
+        
+        list(correlationCoefficient = cor(distributionX, distributionY, method));
+    }
