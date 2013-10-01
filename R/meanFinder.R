2b22b3a746047f064784116c67abc9289028ce32
krishna221 <krishna.prasath.god@gmail.com>
2013-10-01 10:50:17 +0200

    Update meanFinder.R

--- R/meanFinder.R
+++ R/meanFinder.R
@@ -1,8 +1,6 @@
     meanFinder <- function(distribution = "") {
-        if(distribution == "")
-        {
-            distribution = rnorm(50, mean=1, sd=1.5)
-        }
+         distribution = rnorm(50, mean=1, sd=1.5)
+        
         
          message = mean(distribution)
          print(message)
