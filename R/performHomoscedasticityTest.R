8253962421d556130525ff12f2a78ee2a45fe9a6
krishna221 <krishna.prasath.god@gmail.com>
2013-10-02 16:11:46 +0200

    Create performHomoscedasticityTest.R

---
+++ R/performHomoscedasticityTest.R
@@ -0,0 +1,40 @@
+performHomoscedasticityTest <- function(dependentVariable = "", independentVariable = "", dataset = "", type = "levene")
+{
+  install.packages("lawstat");
+  
+  library(lawstat);
+  
+  # Get distributions
+  
+  if(dataset == "")
+  {   
+    y1 = c(18.2, 20.1, 17.6, 16.8, 18.8, 19.7, 19.1);
+    y2 = c(17.4, 18.7, 19.1, 16.4, 15.9, 18.4, 17.7);
+    
+    dependentVariable = c(y1, y2);
+    
+    independentVariable = rep(c('category 1', 'category 2'), each=7);
+    
+    dataset = data.frame(depV = dependentVariable, indepV = factor(independentVariable));
+  }
+  else
+  {
+    dataset = eval(parse(text = dataset));
+    
+    dependentVariable = eval(parse(text = paste(dataset,"$",dependentVariable)));
+    independentVariable = eval(parse(text = paste(dataset,"$",independentVariable)));
+  }  
+    
+  if(type == "levene")
+  {
+    method = "mean"; 
+  }
+  else
+  {
+    method = "median";
+  }
+  
+  result <- levene.test(depVindepV, dataset, paired = paired);
+  
+  list(W = result$statistic[["W"]], p = result$p.value);
+}
