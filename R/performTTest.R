fb1e35750b925bde3d56a0103795a16baa29c9be
krishna221 <krishna.prasath.god@gmail.com>
2013-10-02 11:50:04 +0200

    Create performTTest.R

---
+++ R/performTTest.R
@@ -0,0 +1,29 @@
+performTTest <- function(dataset = "", columnNameX = "", columnNameY = "", paired = "FALSE", tailed = "two.sided", alpha = "0.95")
+{
+  # Get distributions
+  
+  if(dataset == "")
+  {   
+    dataset = "beaver1";
+    columnNameX = "time";
+    columnNameY = "temp";
+  }
+  else if(columnNameX == "")
+  {
+    # Load the first column name by default
+    columnNameX = names(eval(parse(text = dataset)))[1]; 
+    columnNameY = names(eval(parse(text = dataset)))[2]; 
+  }
+        
+  distributionX = eval(parse(text = paste(dataset,"$",columnNameX)));
+  distributionY = eval(parse(text = paste(dataset,"$",columnNameY)));
+  
+  # Do T-test
+  
+  result = t.test(x=distributionX, y=distributionY, alternative=tailed, paired=eval(parse(text = paired)), conf.level=eval(parse(text = alpha)))
+  
+  
+  # Interpret T-test
+  
+  list(p=result$p.value, t=result$statistic, DOF=result$parameter, CI_mean=result$conf.int, method=result$method);
+}
