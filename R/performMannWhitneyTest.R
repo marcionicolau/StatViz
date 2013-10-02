55adb6257038bae45d82ecd42a27cda7a4941257
krishna221 <krishna.prasath.god@gmail.com>
2013-10-02 15:56:19 +0200

    Create performMannWhitneyTest.R

---
+++ R/performMannWhitneyTest.R
@@ -0,0 +1,27 @@
+performMannWhitneyTest <- function(dependentVariable = "", independentVariable = "", dataset = "")
+{
+  # Get distributions
+  
+  if(dataset == "")
+  {   
+    y1 = c(18.2, 20.1, 17.6, 16.8, 18.8, 19.7, 19.1);
+    y2 = c(17.4, 18.7, 19.1, 16.4, 15.9, 18.4, 17.7);
+    
+    dependentVariable = c(y1, y2);
+    
+    independentVariable = rep(c('category 1', 'category 2'), each=7);
+    
+    dataset = data.frame(depV = dependentVariable, indepV = factor(independentVariable));
+  }
+  else
+  {
+    dataset = eval(parse(text = dataset));
+    
+    dependentVariable = eval(parse(text = paste(dataset,"$",dependentVariable)));
+    independentVariable = eval(parse(text = paste(dataset,"$",independentVariable)));
+  }  
+    
+  result <- wilcox.test(depV ~ indepV, dataset);
+  
+  list(W = result$statistic[["W"]], DOF = result$parameter[["df"]], p = result$p.value);
+}
