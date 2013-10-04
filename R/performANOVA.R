9f274e140ab3100404c147fd39c0318f0f42cf61
krishna221 <krishna.prasath.god@gmail.com>
2013-10-04 14:33:46 +0200

    Update performANOVA.R

--- R/performANOVA.R
+++ R/performANOVA.R
@@ -26,5 +26,5 @@ performANOVA <- function(dependentVariable = "", independentVariable = "", datas
   
   result <- anova(fit);
   
-  list(F = result[["F value"]][1], DOF = result[["Df"]][1], p = result[["Pr(>F)"]][1]);
+  list(F = result[["F value"]][1], DOF = result[["Df"]][1], p = result[["Pr(>F)"]][1], data = paste(dependentVariable, independentVariable));
 }
