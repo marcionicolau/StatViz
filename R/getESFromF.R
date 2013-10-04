bfaa1db4965552ccbc928a4148f25f966c71dfd4
krishna221 <krishna.prasath.god@gmail.com>
2013-10-04 15:49:08 +0200

    Update getESFromF.R

--- R/getESFromF.R
+++ R/getESFromF.R
@@ -1,4 +1,4 @@
-getESFromF <- function(F = "", n1 = "", n2 = "")
+getESFromF <- function(FVal = "", n1 = "", n2 = "")
 {
   
   install.packages("compute.es");
@@ -6,17 +6,17 @@ getESFromF <- function(F = "", n1 = "", n2 = "")
   library(compute.es);
   
   
-  if(F == "")
+  if(FVal == "")
   {
-    F = "3.14";
+    FVal = "3.14";
     n1 = "40";
     n2 = "40";
   }
   
   
-  result = fes(eval(parse(text = F)), eval(parse(text = n1)), eval(parse(text = n2)));
+  result = fes(eval(parse(text = FVal)), eval(parse(text = n1)), eval(parse(text = n2)));
   
   # result = fes(3.14, 40, 40);
   
-  list(d = result$MeanDifference[["d"]], g = result$MeanDifference[["g"]], r = result$Correlation[["r"]], F = F, n1 = n1, n2 = n2);
+  list(d = result$MeanDifference[["d"]], g = result$MeanDifference[["g"]], r = result$Correlation[["r"]], FVal= FVal, n1 = n1, n2 = n2);
 }
