f20b03161bb1407eef49042d22e18906839a03e1
krishna221 <krishna.prasath.god@gmail.com>
2013-10-04 15:45:44 +0200

    Update getESFromF.R

--- R/getESFromF.R
+++ R/getESFromF.R
@@ -4,7 +4,9 @@ getESFromF <- function(F = "3.14", n1 = "40", n2 = "40")
   
   library(compute.es);
   
-  result = fes(eval(parse(text = F)), eval(parse(text = n1)), eval(parse(text = n2)));
+  # result = fes(eval(parse(text = F)), eval(parse(text = n1)), eval(parse(text = n2)));
+  
+  result = fes(3.14, 40, 40);
   
   list(d = result$MeanDifference[["d"]], g = result$MeanDifference[["g"]], r = result$Correlation[["r"]], F = F, n1 = n1, n2 = n2);
 }
