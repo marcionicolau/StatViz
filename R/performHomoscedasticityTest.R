7c59994f565a5045b910ee6eb48785e2065d7fa6
krishna221 <krishna.prasath.god@gmail.com>
2013-10-04 14:19:02 +0200

    Update performHomoscedasticityTest.R

--- R/performHomoscedasticityTest.R
+++ R/performHomoscedasticityTest.R
@@ -36,5 +36,5 @@ performHomoscedasticityTest <- function(dependentVariable = "", independentVaria
   
   result <- levene.test(dataset$depV, dataset$indepV, location = method);
   
-  list(testStatistic = result$statistic[["Test Statistic"]], p = result$p.value, method = result$method, data = dataset);
+  list(testStatistic = result$statistic[["Test Statistic"]], p = result$p.value, method = result$method, data = data.frame(depV = dependentVariable, indepV = factor(independentVariable)));
 }
