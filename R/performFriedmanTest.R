6772fdb22d1cf7cbbdc89b33b45289f380a2935b
krishna221 <krishna.prasath.god@gmail.com>
2013-10-07 14:06:46 +0200

    Create performFriedmanTest.R

---
+++ R/performFriedmanTest.R
@@ -0,0 +1,17 @@
+performFriedmanTest <- function(dataset = "")
+{
+  url = "http://ww2.coastal.edu/kingw/statistics/R-tutorials/text/groceries.txt";
+  
+  groceries = read.table(url, header=T);
+  groceries2 <- stack(groceries);
+  subject = rep(groceries$subject,4);
+  
+  groceries2[3] = subject;
+  
+  colnames(groceries2) = c("price","store","subject");
+  
+  result = friedman.test(price ~ store | subject, data = groceries2);
+  
+  list(ChiSquared = result$statistic[["Friedman chi-squared"]], DF = result$parameter[["df"]], p = result$p.value, data = groceries2);
+}
+  
