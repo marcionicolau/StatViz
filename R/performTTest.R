1e97e631a0fbdc1400f2ccd851a485184921a7fe
krishna221 <krishna.prasath.god@gmail.com>
2013-10-07 15:31:04 +0200

    Update performTTest.R

--- R/performTTest.R
+++ R/performTTest.R
@@ -27,5 +27,5 @@ performTTest <- function(dataset = "", columnNameX = "", columnNameY = "", paire
   
   # Interpret T-test
   
-  list(p=result$p.value, t=result$statistic[["t"]], DOF=result$parameter[["df"]], CI_mean=result$conf.int, method=result$method, dataset = dataset, columnName1 = columnNameX, columnName2 = columnNameY);
+  list(p=result$p.value, t=result$statistic[["t"]], DOF=result$parameter[["df"]], CI_mean=result$conf.int, method=result$method, dataset = dataset, columnName1 = columnNameX, columnName2 = columnNameY, alpha = alpha);
 }
