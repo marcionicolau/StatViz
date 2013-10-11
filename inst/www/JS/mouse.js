5dc7632f59c19ffdb84b90413b2902d65c32395b
krishna221 <krishna.prasath.god@gmail.com>
2013-10-11 17:08:11 +0200

    added variables needed for mouse events

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -1,3 +1,12 @@
+var _startX = 0;            // mouse starting positions
+var _startY = 0;
+var _offsetX = 0;           // current element offset
+var _offsetY = 0;
+var _height = 0;
+var _width = 0;
+var _dragElement;           // needs to be passed from OnMouseDown to OnMouseMove
+var _oldZIndex = 0;         // we temporarily increase the z-index during drag		
+
 function InitMouseGestures()
 {
     document.onmousedown = OnMouseDown;
