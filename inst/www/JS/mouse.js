3b2dc1e504de5604e01661452eb6b7b2e5b0edfb
krishna221 <krishna.prasath.god@gmail.com>
2013-10-11 17:30:00 +0200

    tried something

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -63,7 +63,7 @@ function OnMouseOver(e)
     {		
         setup(e, target);
         
-        var variableNameHolder = d3.select("body");
+        var variableNameHolder = d3.select("#0.variableNameHolder");
 //         variableNameHolder.style("cursor","pointer");
         
         console.log("aloha there");
