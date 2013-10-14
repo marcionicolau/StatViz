e3f19d22d8d505915a8db60b5dab908eeff7a724
krishna221 <krishna.prasath.god@gmail.com>
2013-10-14 15:01:10 +0200

    done implementation of histogram

--- inst/www/JS/histogram.js
+++ inst/www/JS/histogram.js
@@ -8,15 +8,24 @@ function makeHistogram(variableName)//nbins
     var max = Array.max(data);
     
     // Should be changeable
-    var bins = 10;
-    var slice = (max - min)/bins;
+    var nBins = 10;
+    var slice = (max - min)/nBins;
     
     console.log("min=" + min + "; max=" + max +"; slice=" + slice);
     
-    for(var i=0; i<data.length; i++)
+    var bins = [];
+    
+    for(var i=0; i<nBbins; i++)
     {
-        console.log((data[i] - min)/slice + "for " + data[i]);
+        bins[i] = 0;
+    }   
+    
+    for(var i=0; i<data.length; i++)
+    { 
+        bins[Math.ceil((data[i] - min)/slice)]++;
     }
+    
+    console.log("histogram: " + bins);
 }
 
 Array.max = function( array )
