d14f24067d032ca31ad755cc617983f8b5632f30
krishna221 <krishna.prasath.god@gmail.com>
2013-10-14 14:57:12 +0200

    histogram implementation ic

--- inst/www/JS/histogram.js
+++ inst/www/JS/histogram.js
@@ -1,6 +1,30 @@
-function makeHistogram(variableName)
+function makeHistogram(variableName)//nbins
 {
     var data = variables[variableName];
     
-
+    var canvas = d3.select("#svgCanvas");
+    
+    var min = Array.min(data);
+    var max = Array.max(data);
+    
+    // Should be changeable
+    var bins = 10;
+    var slice = (max - min)/bins;
+    
+    console.log("min=" + min + "; max=" + max +"; slice=" + slice);
+    
+    for(var i=0; i<data.length; i++)
+    {
+        console.log((data - min)/slice + "for " + data[i]);
+    }
 }
+
+Array.max = function( array )
+ {
+         return Math.max.apply( Math, array );
+ }
+
+ Array.min = function( array )
+ {
+     return Math.min.apply( Math, array );
+ }	
