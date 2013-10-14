e5b71c51274719ef3bb5e35b75b2488145be3cd6
krishna221 <krishna.prasath.god@gmail.com>
2013-10-14 14:19:04 +0200

    ts

--- inst/www/JS/histogram.js
+++ inst/www/JS/histogram.js
@@ -2,10 +2,5 @@ function makeHistogram(variableName)
 {
     var data = variables[variableName];
     
-    var svgCanvas = d3.select("body").append("svg");
-    
-    svgCanvas.attr("left", width*0.2 + "px")
-                    .attr("top", 0 + "px")
-                    .attr("height", height*(2/3))
-                    .attr("width", width*0.8);
+
 }
