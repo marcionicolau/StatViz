485d72b97e9631320cf37a3d96ba332eb65c80b5
krishna221 <krishna.prasath.god@gmail.com>
2013-10-14 14:16:54 +0200

    bug correction

--- inst/www/JS/histogram.js
+++ inst/www/JS/histogram.js
@@ -4,8 +4,8 @@ function makeHistogram(variableName)
     
     var svgCanvas = d3.select("body").append("svg");
     
-    svgCanvas.attr("x", width*0.2)
-                    .attr("y", 0)
+    svgCanvas.attr("left", width*0.2 + "px")
+                    .attr("top", 0 + "px")
                     .attr("height", height*(2/3))
                     .attr("width", width*0.8);
 }
