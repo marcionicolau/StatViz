f9c4153a11e73b3ef5037bcd2e92a21f3dae1f74
krishna221 <krishna.prasath.god@gmail.com>
2013-10-15 11:15:02 +0200

    commented out some stuff. fingers crossed

--- inst/www/JS/histogram.js
+++ inst/www/JS/histogram.js
@@ -19,10 +19,10 @@ function makeHistogram()//nbins
     // Should be changeable
     var nBins = 15;   
     var size = 500;
-    var nGroovesX = 10;
+    var nGroovesX = 15;
     
     
-    console.log("data:{" + data +"}, \nmin: " + min + ", \nmax: " + max + "\n\n");
+//     console.log("data:{" + data +"}, \nmin: " + min + ", \nmax: " + max + "\n\n");
      
     var slice = (max - min)/nBins;    
     
@@ -43,7 +43,7 @@ function makeHistogram()//nbins
     {
         for(var j=0; j<data[i].length; j++)
         { 
-            console.log(Math.ceil((data[i][j] - min)/slice) + ", data=" + data[i][j]);
+//             console.log(Math.ceil((data[i][j] - min)/slice) + ", data=" + data[i][j]);
             
             var index = Math.ceil((data[i][j] - min)/slice);
             
@@ -55,72 +55,77 @@ function makeHistogram()//nbins
     }
     
     console.dir(bins);
+    var binMaxs = new Array();
     
-//     var nGroovesY = Array.max(bins) > 10 ? 10 : Array.max(bins)+1;
-//     
-//     console.log(nGroovesY);
-//     
-//     //draw axes
-//     var xAxis = canvas.append("line")
-//                                     .attr("x1", canvasWidth/2 - size/2)
-//                                     .attr("y1", canvasHeight/2 - size/2)
-//                                     .attr("x2", canvasWidth/2 - size/2)
-//                                     .attr("y2", canvasHeight/2 + size/2)
-//                                     .attr("stroke", "black")
-//                                     .attr("id", "xAxis")
-//                                     .attr("class", "axes");
-//     
-//     var yAxis = canvas.append("line")
-//                                     .attr("x1", canvasWidth/2 - size/2)
-//                                     .attr("y1", canvasHeight/2 + size/2)
-//                                     .attr("x2", canvasWidth/2 + size/2)
-//                                     .attr("y2", canvasHeight/2 + size/2)
-//                                     .attr("stroke", "black")
-//                                     .attr("id", "yAxis")
-//                                     .attr("class", "axes");
-//                                     
-//     var xStep = size/nGroovesX;
-//     
-//     //grooves
-//     for(i=0; i<=nGroovesX; i++)
-//     {
-//         canvas.append("line")
-//                     .attr("x1", canvasWidth/2 - size/2 + i*xStep)
-//                     .attr("y1", canvasHeight/2 + size/2 - 5)
-//                     .attr("x2", canvasWidth/2 - size/2 + i*xStep)
-//                     .attr("y2", canvasHeight/2 + size/2 + 5)
-//                     .attr("id", "groove" + i)
-//                     .attr("class", "xAxisGrooves");
-//         
-//         canvas.append("text")
-//                     .attr("x", canvasWidth/2 - size/2 + i*xStep)
-//                     .attr("y", canvasHeight/2 + size/2 + 35)                    
-//                     .text(Math.round(min + i*slice))
-//                     .attr("id", "groove" + i)
-//                     .attr("class", "xAxisGrooveText");
-//     }
-//     
-//     var yStep = size/(nGroovesY-1);
-//     
-//     for(i=0; i<nGroovesY; i++)
-//     {
-//         canvas.append("line")
-//                     .attr("x1", canvasWidth/2 - size/2 - 5)
-//                     .attr("y1", canvasHeight/2 + size/2 - i*yStep)
-//                     .attr("x2", canvasWidth/2 - size/2 + 5)
-//                     .attr("y2", canvasHeight/2 + size/2 - i*yStep)
-//                     .attr("id", "groove" + i)
-//                     .attr("class", "yAxisGrooves");
-//         
-//         canvas.append("text")
-//                     .attr("x", canvasWidth/2 - size/2 - 35)
-//                     .attr("y", canvasHeight/2 + size/2 - i*yStep + 10)                                        
-//                     .text(0+i)
-//                     .attr("id", "groove" + i)
-//                     .attr("class", "yAxisGrooveText");
-//     }
-//     
-//     //bars
+    for(var i=0; i<currentVariableSelection.length; i++)
+    {
+        binMaxs[i] = Array.max(bins[currentVariableSelection[i]]);
+    }
+            
+    var nGroovesY = Array.max(binMaxs) > 10 ? 10 : Array.max(binMaxs)+1;
+    console.log(nGroovesY);
+    
+    //draw axes
+    var xAxis = canvas.append("line")
+                                    .attr("x1", canvasWidth/2 - size/2)
+                                    .attr("y1", canvasHeight/2 - size/2)
+                                    .attr("x2", canvasWidth/2 - size/2)
+                                    .attr("y2", canvasHeight/2 + size/2)
+                                    .attr("stroke", "black")
+                                    .attr("id", "xAxis")
+                                    .attr("class", "axes");
+    
+    var yAxis = canvas.append("line")
+                                    .attr("x1", canvasWidth/2 - size/2)
+                                    .attr("y1", canvasHeight/2 + size/2)
+                                    .attr("x2", canvasWidth/2 + size/2)
+                                    .attr("y2", canvasHeight/2 + size/2)
+                                    .attr("stroke", "black")
+                                    .attr("id", "yAxis")
+                                    .attr("class", "axes");
+                                    
+    var xStep = size/nGroovesX;
+    
+    //grooves
+    for(i=0; i<=nGroovesX; i++)
+    {
+        canvas.append("line")
+                    .attr("x1", canvasWidth/2 - size/2 + i*xStep)
+                    .attr("y1", canvasHeight/2 + size/2 - 5)
+                    .attr("x2", canvasWidth/2 - size/2 + i*xStep)
+                    .attr("y2", canvasHeight/2 + size/2 + 5)
+                    .attr("id", "groove" + i)
+                    .attr("class", "xAxisGrooves");
+        
+        canvas.append("text")
+                    .attr("x", canvasWidth/2 - size/2 + i*xStep)
+                    .attr("y", canvasHeight/2 + size/2 + 35)                    
+                    .text(Math.round(min + i*slice))
+                    .attr("id", "groove" + i)
+                    .attr("class", "xAxisGrooveText");
+    }
+    
+    var yStep = size/(nGroovesY-1);
+    
+    for(i=0; i<nGroovesY; i++)
+    {
+        canvas.append("line")
+                    .attr("x1", canvasWidth/2 - size/2 - 5)
+                    .attr("y1", canvasHeight/2 + size/2 - i*yStep)
+                    .attr("x2", canvasWidth/2 - size/2 + 5)
+                    .attr("y2", canvasHeight/2 + size/2 - i*yStep)
+                    .attr("id", "groove" + i)
+                    .attr("class", "yAxisGrooves");
+        
+        canvas.append("text")
+                    .attr("x", canvasWidth/2 - size/2 - 35)
+                    .attr("y", canvasHeight/2 + size/2 - i*yStep + 10)                                        
+                    .text(0+i)
+                    .attr("id", "groove" + i)
+                    .attr("class", "yAxisGrooveText");
+    }
+    
+    //bars
 //     for(i=0; i<nBins; i++)
 //     {
 //         canvas.append("rect")
