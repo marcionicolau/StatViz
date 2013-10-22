bf0f6672fa3cb0e9ec064aebc18743c6bf417823
krishna221 <krishna.prasath.god@gmail.com>
2013-10-22 14:07:48 +0200

    ts

--- R/getData.R
+++ R/getData.R
@@ -2,5 +2,5 @@ getData <- function(dataset = "langley", columnName = "Unemployed")
 {
     assign(paste("dataset"),dataset);
     
-    list(DATA = eval(parse(text = paste(dataset,"$",columnName))));
+    list(DATA = eval(parse(text = paste("dataset","$",columnName))));
 }
