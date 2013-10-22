692d780734a3595e2a7b7b8aa14673c7d0070a8f
krishna221 <krishna.prasath.god@gmail.com>
2013-10-22 13:21:09 +0200

    just trying something

--- R/loadFile.R
+++ R/loadFile.R
@@ -1,6 +1,6 @@
 loadFile <- function(filePath)
 {
-    DATA = read.table(filePath, head = T);
+    DATA <- read.table(filePath, head = T);
     
     list(data = "DATA", variableNames = names(DATA));
 }
