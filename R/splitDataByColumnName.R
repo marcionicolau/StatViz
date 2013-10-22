f5307da7640979ec8a11b76d43dcc466344fa195
krishna221 <krishna.prasath.god@gmail.com>
2013-10-22 17:41:42 +0200

    ts

--- R/splitDataByColumnName.R
+++ R/splitDataByColumnName.R
@@ -1,13 +1,10 @@
-splitDataByColumnValues <- function(dataset, columnName)
+splitDataByColumnValues <- function(dataset, columnName, value)
 {
     table <- as.data.frame(dataset);
     
     levels = levels(eval(parse(text = paste("table","$",columnName))));
-    
-    for(i in 1:length(levels))
-    {
-        assign(paste(levels[i]), subset(paste("table"), paste("table","$",columnName) == paste(levels[i])));
-        list(assign(paste(levels[i]),levels[i]));
-    }
+  
+    list(data = subset(paste("table"), paste("table","$",columnName) == value));
+ 
 }  
     
