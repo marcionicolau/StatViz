4d3c164a8ca1a9cec74e5b4555a0d9e7bebc12c3
krishna221 <krishna.prasath.god@gmail.com>
2013-10-22 13:32:08 +0200

    ts

--- R/loadFile.R
+++ R/loadFile.R
@@ -1,6 +1,6 @@
 loadFile <- function(filePath)
 {
-    eval(parse(text(paste("DATA","=","read.table(filePath, head = T)"))));
+    eval(parse(text(paste("DATA",=,read.table(filePath, head = T)))));
     
     list(data = "DATA", variableNames = names(DATA));
 }
