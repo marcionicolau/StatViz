8070f1cd559d00980224d72ba120b39dea23f0c3
krishna221 <krishna.prasath.god@gmail.com>
2013-10-28 13:34:09 +0100

    fixed homoscedasticity test opencpu function

--- R/performHomoscedasticityTest.R
+++ R/performHomoscedasticityTest.R
@@ -4,26 +4,7 @@ performHomoscedasticityTest <- function(dependentVariable = "", independentVaria
   
   library(lawstat);
   
-  # Get distributions
-  
-  if(dataset == "")
-  {   
-    y1 = c(18.2, 20.1, 17.6, 16.8, 18.8, 19.7, 19.1);
-    y2 = c(17.4, 18.7, 19.1, 16.4, 15.9, 18.4, 17.7);
-    
-    dependentVariable = c(y1, y2);
-    
-    independentVariable = rep(c('category 1', 'category 2'), each=7);
-    
-    dataset = data.frame(depV = dependentVariable, indepV = factor(independentVariable));
-  }
-  else
-  {
-    dataset = eval(parse(text = dataset));
-    
-    dependentVariable = eval(parse(text = paste(dataset,"$",dependentVariable)));
-    independentVariable = eval(parse(text = paste(dataset,"$",independentVariable)));
-  }  
+  table <- as.data.frame(dataset) 
     
   if(type == "levene")
   {
@@ -34,7 +15,7 @@ performHomoscedasticityTest <- function(dependentVariable = "", independentVaria
     method = "median";
   }
   
-  result <- levene.test(dataset$depV, dataset$indepV, location = method);
+  result <- eval(parse(text = paste("levene.test(table$",dependentVariable,", table$", independentVariable, ",location = method)")));
   
   list(testStatistic = result$statistic[["Test Statistic"]], p = result$p.value, method = result$method, data = toString(dataset));
 }
