670240e2e2ef30896c5638acf0f046e57e0f956a
krishna221 <krishna.prasath.god@gmail.com>
2013-10-28 17:45:50 +0100

    minor bug fix

--- inst/www/JS/statistical-tests.js
+++ inst/www/JS/statistical-tests.js
@@ -235,7 +235,7 @@ function drawScales(cx, cy)
                 .attr("fill", meanColors["normal"])
                 .attr("id", "tickText")
                 .attr("class", "significanceTestScaleText")
-                .text(testResults["mean"]);
+                .text(testResults["grandMean"]);
     }           
 }   
 
