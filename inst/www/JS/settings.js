ab929ff2edbf2465f53404596cb7b6203b88c34a
krishna221 <krishna.prasath.god@gmail.com>
2013-10-29 09:54:29 +0100

    ts

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -21,7 +21,7 @@ variableSelectionButtonWidth = 50;
 var size = 400;
 var axesOffset = 25; //distance from plots to axes (for an R-like appearance)
 var tickTextOffsetXAxis = 30;
-var tickTextOffsetYAxis = 10;
+var tickTextOffsetYAxis = 20;
 
 //Define colors for histogram bars, color scatterplot
 var colors = ["rgba(255,0,0,0.5)","rgba(0,255,0,0.5)","rgba(0,0,255,0.5)", "rgba(255,255,0,0.5)","rgba(0,255,255,0.5)","rgba(255,0,255,0.5)"];
