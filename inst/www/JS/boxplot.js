68dc7f7c70e0941b6cd6d809684d3f40ec54e48a
krishna221 <krishna.prasath.god@gmail.com>
2013-10-29 18:41:31 +0100

    ts

--- inst/www/JS/boxplot.js
+++ inst/www/JS/boxplot.js
@@ -127,7 +127,7 @@ function makeBoxplot()
     
     if(altBoxPlot == true)    
     {
-        labels = levels;
+        labels = variableList["independent-levels"];
     }    
     else    
     {        
