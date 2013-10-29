192500e687af9d1e001c65fbc2f6e9bed501da4f
krishna221 <krishna.prasath.god@gmail.com>
2013-10-29 10:05:43 +0100

    minor tweaks

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -22,7 +22,7 @@ var size = 400;
 var axesOffset = 25; //distance from plots to axes (for an R-like appearance)
 var tickTextOffsetXAxis = 20;
 var tickTextOffsetYAxis = 15;
-var yAxisTickTextOffset = 8;
+var yAxisTickTextOffset = 6;
 
 //Define colors for histogram bars, color scatterplot
 var colors = ["rgba(255,0,0,0.5)","rgba(0,255,0,0.5)","rgba(0,0,255,0.5)", "rgba(255,255,0,0.5)","rgba(0,255,255,0.5)","rgba(255,0,255,0.5)"];
