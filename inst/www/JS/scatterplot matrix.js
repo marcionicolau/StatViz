5f8e4bf508dcac74837460cb5199e639e91f4787
krishna221 <krishna.prasath.god@gmail.com>
2013-10-30 14:44:00 +0100

    fixed scatterplot matrix too!

--- inst/www/JS/scatterplot matrix.js
+++ inst/www/JS/scatterplot matrix.js
@@ -26,13 +26,13 @@ function makeScatterplotMatrix()
             for(var j=0; j<numberOfVariables; j++)
             {
                 if(i != j)
-                    makeScatterPlotAt(canvasWidth/2 - size/2 + j*(size/2), canvasHeight/2 + i*(size/2), (size/numberOfVariables) - axesOffset, variableList["dependent"][i], variableList["dependent"][j]); 
+                    makeScatterPlotAt(canvasWidth/2 - plotWidth/2 + j*(plotWidth/2), canvasHeight/2 + i*(plotHeight/2), (plotWidth/numberOfVariables) - axesOffset, (plotHeight/2) - axesOffset, variableList["dependent"][i], variableList["dependent"][j]); 
             }
         }
     }
 }
 
-function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
+function makeScatterPlotAt(x,y,shortWidth, shortHeight, variableX, variableY, variableColor)
 {
     // make sure that all preprocessing is done in the makeScatterPlotMatrix() function
     
@@ -52,7 +52,7 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
     canvas.append("line")
             .attr("x1", x)
             .attr("y1", y + shortAxesOffset)
-            .attr("x2", x + plotSize)
+            .attr("x2", x + shortWidth)
             .attr("y2", y + shortAxesOffset)
             .attr("stroke", "black")
             .attr("id", "axis")
@@ -63,12 +63,14 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
             .attr("x1", x - shortAxesOffset)
             .attr("y1", y)
             .attr("x2", x - shortAxesOffset)
-            .attr("y2", y - plotSize)
+            .attr("y2", y - shortHeight)
             .attr("stroke", "black")
             .attr("id", "axis")
             .attr("class", "yAxis");
     
-    var step = plotSize/(shortNumberOfGrooves-1);
+    var xStep = shortWidth/(shortNumberOfGrooves-1);
+    var yStep = shortHeight/(shortNumberOfGrooves-1);
+    
     var xSlice = (maxX - minX)/(shortNumberOfGrooves-1);    
     var ySlice = (maxY - minY)/(shortNumberOfGrooves-1);    
     
@@ -78,15 +80,15 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
     for(i=0; i<shortNumberOfGrooves; i++)
     {
         canvas.append("line")
-                    .attr("x1", x + i*step)
+                    .attr("x1", x + i*xStep)
                     .attr("y1", y + shortAxesOffset)
-                    .attr("x2", x + i*step)
+                    .attr("x2", x + i*xStep)
                     .attr("y2", y + shortAxesOffset + shortTickLength)
                     .attr("id", "groove" + i)
                     .attr("class", "xAxisGrooves");
         
         canvas.append("text")
-                    .attr("x", x + i*step)
+                    .attr("x", x + i*xStep)
                     .attr("y", y + shortAxesOffset + tickTextOffsetXAxis)     
                     .attr("font-size", fontSize)
                     .text(format(minX + i*xSlice))
@@ -99,15 +101,15 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
     {
         canvas.append("line")
                     .attr("x1", x - shortAxesOffset)
-                    .attr("y1", y - i*step)
+                    .attr("y1", y - i*yStep)
                     .attr("x2", x - shortAxesOffset - shortTickLength)
-                    .attr("y2", y - i*step)
+                    .attr("y2", y - i*yStep)
                     .attr("id", "groove" + i)
                     .attr("class", "yAxisGrooves");
         
         canvas.append("text")
                     .attr("x", x - shortAxesOffset - tickTextOffsetYAxis)
-                    .attr("y", y - i*step)  
+                    .attr("y", y - i*xStep)  
                     .text(format(minY + i*ySlice))
                     .attr("font-size", fontSize + "px")
                     .attr("text-anchor", "end")
@@ -120,8 +122,8 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
         var color = "black";
         
         canvas.append("circle")
-                    .attr("cx", x + getValue(dataX[i], minX, maxX)*plotSize)
-                    .attr("cy", y - getValue(dataY[i], minY, maxY)*plotSize)
+                    .attr("cx", x + getValue(dataX[i], minX, maxX)*shortWidth)
+                    .attr("cy", y - getValue(dataY[i], minY, maxY)*shortHeight)
                     .attr("r", shortDataPointRadius)
                     .attr("fill", color)
                     .attr("id", "data" + i)
