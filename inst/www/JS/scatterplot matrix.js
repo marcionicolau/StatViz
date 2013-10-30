a6f16c51c379b74493c7e6e7fe6c963a8b415a44
krishna221 <krishna.prasath.god@gmail.com>
2013-10-30 13:20:44 +0100

    ts

--- inst/www/JS/scatterplot matrix.js
+++ inst/www/JS/scatterplot matrix.js
@@ -28,6 +28,22 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
     
     var canvas = d3.select("#svgCanvas");
     
+    canvas.append("rect")
+            .attr("x", x)
+            .attr("y", y - plotSize)
+            .attr("height", plotSize)
+            .attr("width", plotSize)
+            .attr("stroke", "yellow")
+            .attr("fill", "none");
+    
+    var maxX = MAX[variableX];
+    var minX = MIN[variableX];
+    
+    var maxY = MAX[variableY];
+    var minY = MIN[variableY];
+    
+    console.log("x:[" + minX + "," + maxX + "\ny:[" + minY + "," + maxY + "]");
+    
     // x-axis
     canvas.append("line")
             .attr("x1", x)
@@ -48,7 +64,50 @@ function makeScatterPlotAt(x,y,plotSize, variableX, variableY, variableColor)
             .attr("id", "axis")
             .attr("class", "yAxis");
             
+    var nGrooves = 10;
     
+    var step = plotSize/(nGrooves-1);
+    var xSlice = (maxX - minX)/(nGrooves-1);    
+    var ySlice = (maxY - minY)/(nGrooves-1);    
+    
+    //grooves
+    for(i=0; i<nGrooves; i++)
+    {
+        canvas.append("line")
+                    .attr("x1", x + i*step)
+                    .attr("y1", y)
+                    .attr("x2", x + i*step)
+                    .attr("y2", y + 10)
+                    .attr("id", "groove" + i)
+                    .attr("class", "xAxisGrooves");
+        
+        canvas.append("text")
+                    .attr("x", x + i*step)
+                    .attr("y", y)                    
+                    .text(format(minX + i*xSlice))
+                    .attr("text-anchor", "middle")
+                    .attr("id", "groove" + i)
+                    .attr("class", "xAxisGrooveText");
+    }
+    
+    for(i=0; i<nGrooves; i++)
+    {
+        canvas.append("line")
+                    .attr("x1", x)
+                    .attr("y1", y + i*step)
+                    .attr("x2", x - 10)
+                    .attr("y2", y + i*step)
+                    .attr("id", "groove" + i)
+                    .attr("class", "yAxisGrooves");
+        
+        canvas.append("text")
+                    .attr("x", x)
+                    .attr("y", y + i*step)  
+                    .text(format(minY + i*ySlice))
+                    .attr("text-anchor", "end")
+                    .attr("id", "groove" + i)
+                    .attr("class", "yAxisGrooveText");
+    }
 }
 
 function getValue(number, type)
