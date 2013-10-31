82e6da1a8d1ccb8cd084d87ae3b26646176858bc
krishna221 <krishna.prasath.god@gmail.com>
2013-10-31 17:53:37 +0100

    ts

--- R/findTransform.R
+++ R/findTransform.R
@@ -3,27 +3,39 @@ findTransform <- function(distribution)
     distribution = c(distribution);
 
     result = eval(parse(text = paste("shapiro.test(sqrt(distribution))")));
-    if(result$p.value > 0.05)
+    if(!is.nan(result$p.value))    
     {
-        type = "sqrt";
+        if(result$p.value > 0.05)
+        {
+            type = "sqrt";
+        }
     }
 
     result = eval(parse(text = paste("shapiro.test(distribution^(1/3))")));
-    if(result$p.value > 0.05)
+    if(!is.nan(result$p.value))    
     {
-      type = "cube";
+        if(result$p.value > 0.05)
+        {
+            type = "cube";
+        }
     }
 
     result = eval(parse(text = paste("shapiro.test(1/distribution)")));
-    if(result$p.value > 0.05)
+    if(!is.nan(result$p.value))    
     {
-      type = "reciprocal";
+        if(result$p.value > 0.05)
+        {
+            type = "reciprocal";
+        }
     }
 
     result = eval(parse(text = paste("shapiro.test(log10(distribution))")));
-    if(result$p.value > 0.05)
+    if(!is.nan(result$p.value))    
     {
-      type =  "log";
+        if(result$p.value > 0.05)
+        {
+            type = "log";
+        }
     }
 
     type = "none";
