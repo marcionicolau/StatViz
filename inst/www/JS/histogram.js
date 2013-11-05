db8b2eb427babd98f5d7c2de18fba478c5e6ff15
krishna221 <krishna.prasath.god@gmail.com>
2013-11-05 10:57:32 +0100

    ts

--- inst/www/JS/histogram.js
+++ inst/www/JS/histogram.js
@@ -605,7 +605,7 @@ function drawLegends(varNames)
         
         canvas.append("text")
                 .attr("x", RIGHT + 2*histLegendOffsetX + histLegendSize)
-                .attr("y", TOP + histLegendOffsetY + i*yStep + histLegendSize/2 - 3)
+                .attr("y", TOP + histLegendOffsetY + i*yStep + histLegendSize/2)
                 .attr("fill", "black")
                 .text(varNames[i])
                 .attr("id", "legend" + i)
