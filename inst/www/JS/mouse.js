46814f6cae287e66e394154c8189f3b3419c56b6
krishna221 <krishna.prasath.god@gmail.com>
2013-11-05 18:36:14 +0100

    ts

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -113,7 +113,27 @@ function OnMouseDown(e)
     }
     else if((e.button == 1 && window.event != null || e.button == 0) && target.className.baseVal == "fullscreen")
     {
-        setup(e, target);
+        // grab the mouse position
+        _startX = e.clientX;
+        _startY = e.clientY;
+
+        // grab the clicked element's position
+        _offsetX = removeAlphabetsFromString(target.left);
+        _offsetY = removeAlphabetsFromString(target.top);      
+
+        // we need to access the element in OnMouseMove
+        _dragElement = target;
+
+        // tell our code to start moving the element with the mouse
+    //     document.onmousemove = OnMouseMove;
+
+        // cancel out any text selections
+        document.body.focus();
+
+        // prevent text selection in IE
+        document.onselectstart = function () { return false; };
+        // prevent IE from trying to drag an image
+        target.ondragstart = function() { return false; };
         
         var button = d3.select(".fullscreen");
         
@@ -320,7 +340,27 @@ function OnMouseOver(e)
     }
     else if(target.className.baseVal == "fullscreen")
     {
-        setup(e.target);
+        // grab the mouse position
+        _startX = e.clientX;
+        _startY = e.clientY;
+
+        // grab the clicked element's position
+        _offsetX = removeAlphabetsFromString(target.left);
+        _offsetY = removeAlphabetsFromString(target.top);      
+
+        // we need to access the element in OnMouseMove
+        _dragElement = target;
+
+        // tell our code to start moving the element with the mouse
+    //     document.onmousemove = OnMouseMove;
+
+        // cancel out any text selections
+        document.body.focus();
+
+        // prevent text selection in IE
+        document.onselectstart = function () { return false; };
+        // prevent IE from trying to drag an image
+        target.ondragstart = function() { return false; };
         
         var button = d3.select(".fullscreen");
         button.attr("cursor", "pointer");
@@ -383,15 +423,12 @@ function setup(e, target)
     _startY = e.clientY;
 
     // grab the clicked element's position
-    if(target.style != undefined)
-    {
-        _offsetX = removeAlphabetsFromString(target.style.left);
-        _offsetY = removeAlphabetsFromString(target.style.top);      
-        target.style.zIndex = +1;
-    }
+    _offsetX = removeAlphabetsFromString(target.style.left);
+    _offsetY = removeAlphabetsFromString(target.style.top);      
+
     // bring the clicked element to the front while it is being dragged
     _oldZIndex = target.style.zIndex;
-
+    target.style.zIndex = +1;
 
     // we need to access the element in OnMouseMove
     _dragElement = target;
