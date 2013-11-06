2a9d9be0dc811cdc2644337982f99b6510e8dbd4
krishna221 <krishna.prasath.god@gmail.com>
2013-11-06 11:14:09 +0100

    ts

--- inst/www/JS/opencpu-R.js
+++ inst/www/JS/opencpu-R.js
@@ -155,12 +155,9 @@ function getCI(dataset, variableName, level)
                   }, function(output) {                  
         console.log("CI for " + variableName + "[" + level + "] = " + output.min + ", " + output.max);                
         
-        if(CI[variableName][level] = undefined)
-        {
-            console.log("hey");
         
-            CI[variableName][level] = new Array();
-        }
+        CI[variableName][level] = new Array();
+        
         
         CI[variableName][level].push(1);
         console.log(CI[variableName][level]);
