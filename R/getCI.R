1ced6b5fc07368df5125c304142e10ea0490f35b
krishna221 <krishna.prasath.god@gmail.com>
2013-11-06 11:39:49 +0100

    fb with sa

--- R/getCI.R
+++ R/getCI.R
@@ -9,7 +9,7 @@ getCI <- function(dataset, variableName, alpha = "0.95")
   n = length(distribution);
     
   alpha = eval(parse(text = alpha));
-  z = 1 - (alpha/2);
+  z = (1 - alpha)/2;
   
   error <- qnorm(z)*sigma/sqrt(n);
   
