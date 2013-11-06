e3effe87efeadcf37c845d4c5710fa97b44b00aa
krishna221 <krishna.prasath.god@gmail.com>
2013-11-06 14:02:35 +0100

    ts

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -413,13 +413,13 @@ function OnMouseOver(e)
                 .attr("x",(parseFloat(bottomFringe.attr("x1")) + parseFloat(bottomFringe.attr("x2")))/2)
                 .attr("y", bottomFringe.attr("y1") - 10)
                 .attr("text-anchor", "middle")
-                .text(CI[variableList["dependent"][0]][target.id]);
+                .text(CI[variableList["dependent"][0]][target.id][0]);
         
         canvas.append("text")
                 .attr("x",(parseFloat(topFringe.attr("x1")) + parseFloat(topFringe.attr("x2")))/2)
                 .attr("y", topFringe.attr("y1") + 30)
                 .attr("text-anchor", "middle")
-                .text(CI[variableList["dependent"][1]][target.id]);
+                .text(CI[variableList["dependent"][0]][target.id][1]);
                 
         
         topLine.transition().duration(1000)
