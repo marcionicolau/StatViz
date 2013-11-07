7bc55dd4efe55bd3b7bdc3ea1c31a48dc6db84bf
krishna221 <krishna.prasath.god@gmail.com>
2013-11-07 15:50:23 +0100

    ts

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -41,7 +41,7 @@ var buttonColors = new Object();
 
 
 //Define colors for histogram bars, color scatterplot
-var colors = ["rgba(255,0,0,0.05)","rgba(0,255,0,0.05)","rgba(0,0,255,0.05)", "rgba(255,255,0,0.25)","rgba(0,255,255,0.25)","rgba(255,0,255,0.25)"];
+var colors = ["rgba(255,0,0,1.0)","rgba(0,255,0,0.75)","rgba(0,0,255,0.5)", "rgba(255,255,0,0.25)","rgba(0,255,255,0.25)","rgba(255,0,255,0.25)"];
 
 var meanColors = new Object(); //Colors for mean, and ?
     meanColors["normal"] = "purple";
