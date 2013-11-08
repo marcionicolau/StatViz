ba95936b16bbebf111ad6e848b782129abe4fb4a
krishna221 <krishna.prasath.god@gmail.com>
2013-11-08 12:55:30 +0100

    ts

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -1,7 +1,7 @@
 // Data
 var dataset = "mtcars";
 
-var fileName = "titanicSurvivors.csv"; //"cars-mod.txt"; //"newDay.txt";"news-sports-business.txt"; 
+var fileName = "IMPACT.txt";//"cars-mod.txt"; //"newDay.txt";"news-sports-business.txt"; 
 var pathToFile = "/Users/krishnasubramanian/Documents/Media Informatics/Semester 4/Thesis/Implementation/Possible Datasets/Datasets/" + fileName;
 
 var experimentalDesign = "between-groups";
