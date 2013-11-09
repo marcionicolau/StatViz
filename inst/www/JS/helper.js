7da4bc38d10aca799fc436d4de00ab55928e8700
krishna221 <krishna.prasath.god@gmail.com>
2013-11-09 17:56:57 +0100

    ts

--- inst/www/JS/helper.js
+++ inst/www/JS/helper.js
@@ -224,12 +224,12 @@ function toggleFillColorsForVisualizations()
                     switch(variableList["dependent"].length)
                     {
                         case 1:
-                                invalidate(viz[3], viz[4]);
+                                invalidate(viz[3]);
                                 break;
                         case 2:
                                 break;
                         default:
-                                invalidate(viz[3], viz[4]);
+                                invalidate(viz[3]);
                     }
                     
                     break;
@@ -259,6 +259,7 @@ function validateAll()
     for(var i=0; i<visualizations.length; i++)
     {        
         visualizations[i].setAttribute("class", "visualizationHolder");         
+        visualizations[i].setAttribute("opacity", "1.0");
     }
 }
 
@@ -272,6 +273,7 @@ function invalidate(list)
         if(visualizations[i].getAttribute("id").indexOf(list) != -1)
         {
             visualizations[i].setAttribute("class", "invalid");
+            visualizations[i].setAttribute("opacity", "0.25");
         }        
     }
 }
