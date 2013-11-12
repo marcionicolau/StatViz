8a6e76baeec49079b2294cf20322959f542a30c3
krishna221 <krishna.prasath.god@gmail.com>
2013-11-12 11:42:17 +0100

    ts

--- inst/www/JS/mouse.js
+++ inst/www/JS/mouse.js
@@ -577,8 +577,8 @@ function OnMouseOver(e)
         
         var slopeOfRegressionLine = (regressionLine.attr("y2") - regressionLine.attr("y1"))/(regressionLine.attr("x2") - regressionLine.attr("x1"));
                 
-        mouseX = canvasWidth/2;//(canvasHeight-mouseY)*slopeOfRegressionLine + interceptCircle.attr("cy");
-        mouseY = toModifiedViewBoxForRegressionLineYCoordinate((mouseX - (width-canvasWidth) - interceptCircle.attr("cy"))/slopeOfRegressionLine);
+        mouseX = e.pageX - (width - canvasWidth);//(canvasHeight-mouseY)*slopeOfRegressionLine + interceptCircle.attr("cy");
+        mouseY = (mouseX - interceptCircle.attr("cy"))/slopeOfRegressionLine;
         
         canvas.append("circle")
                 .attr("cx", mouseX)
