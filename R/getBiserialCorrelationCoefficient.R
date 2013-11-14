bf4cab4a53b37461fbe78eb6741247923c2bde67
krishna221 <krishna.prasath.god@gmail.com>
2013-11-14 16:04:24 +0100

    ts

--- R/getBiserialCorrelationCoefficient.R
+++ R/getBiserialCorrelationCoefficient.R
@@ -3,7 +3,7 @@ getBiserialCorrelationCoefficient <- function(continuousVariable, binaryVariable
     require("ltm");
     
     c <- c(continuousVariable);
-    b <- b(continuousVariable);
+    b <- c(continuousVariable);
     
     list(cor = biserial.cor(c, b, use = "complete.obs"));  
 }
