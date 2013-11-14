3b22c55f624ee77b49046e743dc3466a9444d1cb
krishna221 <krishna.prasath.god@gmail.com>
2013-11-14 15:39:09 +0100

    ts

--- inst/www/JS/settings.js
+++ inst/www/JS/settings.js
@@ -1,8 +1,8 @@
 // Data
     localStorage.fileName = "store";
-    var fileName = "cars";//change this!
+    var fileName = "weightLoss";//change this!
     var wd = "/Users/krishnasubramanian/Documents/Media Informatics/Semester 4/Thesis/Implementation/Possible Datasets/Datasets/";//"http://hci.rwth-aachen.de/~subramanian/datasets/" + localStorage.fileName +".txt";
-    var pathToFile =  wd + "cars-mod.txt";
+    var pathToFile =  wd + "twoway.csv";
 
 // Skeleton
     var width = document.width;
