12b2056184e9edb819f0f5fd430f3f6bf2b8a325
krishna221 <krishna.prasath.god@gmail.com>
2013-11-15 16:41:33 +0100

    ts

--- inst/www/JS/tukeyHSDPlot.js
+++ inst/www/JS/tukeyHSDPlot.js
@@ -11,7 +11,7 @@ function drawTukeyHSDPlot()
     
     //data - we have already sorted them into "tukeyResults" object 
     var variableList = sort(currentVariableSelection);    
-    var levels = variables[variableList["independent"][0]]["dataset"].unique();    
+    var levels = variables[variableList["independent"][0]]["dataset"].unique().sort();    
     
     var min = parseFloat(sessionStorage.tukeyResultsMin);
     var max = parseFloat(sessionStorage.tukeyResultsMax);
@@ -37,12 +37,12 @@ function drawTukeyHSDPlot()
     
     //Y-axis label
     canvas.append("text")
-                .attr("x", LEFT - axesOffset - labelOffset)
+                .attr("x", LEFT - axesOffset - 2*labelOffset)
                 .attr("y", (TOP + BOTTOM)/2)
                 .attr("text-anchor", "middle")
                 .attr("font-size", "24px")
                 .text("Mean Difference in " + variableList["dependent"][0])
-                .attr("transform", "rotate(-90 " + (LEFT - axesOffset - labelOffset) + " " + ((TOP + BOTTOM)/2 + 6) + ")")
+                .attr("transform", "rotate(-90 " + (LEFT - axesOffset - 2*labelOffset) + " " + ((TOP + BOTTOM)/2 + 6) + ")")
                 .attr("fill", "orange");
                 
     //X-axis grooves
@@ -94,42 +94,24 @@ function drawTukeyHSDPlot()
                     .attr("id", "groove" + i)
                     .attr("class", "yAxisGrooveText");
     }   
-//     
-//     for(var i=0; i<interactions.length; i++)
-//     {
-//         var x,y;
-//         
-//         x = LEFT + levelsOfIndependentVariableXAxis.indexOf(levelsOfIndependentVariableXAxis[i%levelsOfIndependentVariableXAxis.length])*xStep;
-//         y = BOTTOM - getValue1(interactions[i], min, max)*plotHeight;        
-//         
-//         var color = colors[Math.floor(i/(interactions.length/levelsOfIndependentVariableColor.length))];
-//         
-//         canvas.append("circle")
-//                     .attr("cx", x)
-//                     .attr("cy", y)
-//                     .attr("r", "5px")
-//                     .attr("fill", color)
-//                     .attr("id", "c" + Math.floor(i/(interactions.length/levelsOfIndependentVariableColor.length)) + (i%levelsOfIndependentVariableXAxis.length))
-//                     .attr("class", "effs");     
-//     }
-//     
-//     for(var i=0; i<levelsOfIndependentVariableColor.length; i++)
-//     {
-//         var circles = [];
-//         
-//         for(var j=0; j<levelsOfIndependentVariableXAxis.length; j++)
-//         {
-//             circles.push(d3.select("#c" + i + j + ".effs"));
-//             
-//             if(j != 0)
-//             {
-//                 canvas.append("line")
-//                         .attr("x1", circles[j].attr("cx"))
-//                         .attr("y1", circles[j].attr("cy"))
-//                         .attr("x2", circles[j-1].attr("cx"))
-//                         .attr("y2", circles[j-1].attr("cy"))
-//                         .attr("stroke", colors[i]);
-//             }
-//         }        
-//     }
+     
+    for(var i=0; i<levels.length; i++)
+    {
+        for(var j=i+1; j<levels.length; j++)
+        {
+            if(i != j)
+            {
+                var x,y;
+
+                x = LEFT + (i+j)*xStep;
+                y = BOTTOM - getValue1(tukeyResults[levels[i]][levels[j]]["difference"], min, max)*plotHeight;        
+
+                canvas.append("circle")
+                            .attr("cx", x)
+                            .attr("cy", y)
+                            .attr("r", "5px")
+                            .attr("fill", "magenta")    
+            }
+        }
+    }
 }
