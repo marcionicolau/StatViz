c13d34ace968677c01385de8018603b48985a2fd
krishna221 <krishna.prasath.god@gmail.com>
2013-11-16 14:21:07 +0100

    fixed a minor bug

--- R/performOneSampleTTest.R
+++ R/performOneSampleTTest.R
@@ -6,7 +6,7 @@ performOneSampleTTest <- function(distribution, trueMean = "0")
     
     require(powerAnalysis);
     
-    d = ES.t.one(n = length(distribution), mu = estimate = result$estimate[["mean of x"]], t = result$statistic[["t"]])[["d"]];
+    d = ES.t.one(n = length(distribution), mu = result$estimate[["mean of x"]], t = result$statistic[["t"]])[["d"]];
     
     list(t = result$statistic[["t"]], df = result$parameter[["df"]], p = result$p.value, estimate = result$estimate[["mean of x"]], method = result$method, d = d);
 }
