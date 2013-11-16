bca9a32bd3d6e36df651dee8d6be70145dafdfec
krishna221 <krishna.prasath.god@gmail.com>
2013-11-16 11:34:12 +0100

    modified how biserial correlation coefficient works

--- inst/www/JS/statistics-helper.js
+++ inst/www/JS/statistics-helper.js
@@ -21,29 +21,29 @@ function findCorrelationCoefficient(variableA, variableB, noDisplay)
         if(variableDataTypes[variableA] == "binary")
         {
             console.log(isNaN(variables[variableA]["dataset"][0]));
-            if(!isNaN(variables[variableA]["dataset"][0]))
-            {
+//             if(!isNaN(variables[variableA]["dataset"][0]))
+//             {
                 console.log("\t\t\tBiserial Correlation Coefficient");
                 getBiserialCorrelationCoefficient(variableB, variableA, noDisplay);
-            }
-            else
-            {   
-                console.log("\t\t\tDoing nothing");
-                return -1;
-            }
+            // }
+//             else
+//             {   
+//                 console.log("\t\t\tDoing nothing");
+//                 return -1;
+//             }
         }
         else
         {
-            if(!isNaN(variables[variableB]["dataset"][0]))
-            {
+            // if(!isNaN(variables[variableB]["dataset"][0]))
+//             {
                 console.log("\t\t\tBiserial Correlation Coefficient");
                 getBiserialCorrelationCoefficient(variableA, variableB, noDisplay);
-            }
-            else
-            {
-                console.log("\t\t\tDoing nothing");
-                return -1;
-            }            
+            // }
+//             else
+//             {
+//                 console.log("\t\t\tDoing nothing");
+//                 return -1;
+//             }            
         }
     }
     else
