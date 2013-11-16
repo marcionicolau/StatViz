2ffaa2c156bc167cc208baa4e7252c154139c6eb
krishna221 <krishna.prasath.god@gmail.com>
2013-11-16 23:09:14 +0100

    ts

--- inst/www/JS/statistical-tests.js
+++ inst/www/JS/statistical-tests.js
@@ -440,8 +440,16 @@ function displaySignificanceTestResults()
     
     sideBar.append("circle")
             .attr("cx", sideBarWidth/2)
-            .attr("cy", significanceTestResultOffset)
-            .attr("r", 20)
+            .attr("cy", 2*significanceTestResultOffset)
+            .attr("r", 50)
+            .attr("id", "tukey")
+            .attr("class", "significance"); 
+    
+    sideBar.append("text")
+            .attr("x", sideBarWidth/2)
+            .attr("y", 2*significanceTestResultOffset-6)
+            .attr("fill", "white")
+            .text("Tukey's HSD")
             .attr("id", "tukey")
             .attr("class", "significance"); 
 }
