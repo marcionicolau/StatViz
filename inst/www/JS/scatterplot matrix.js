447df0305b675082b4317226b8b33af2a98ae0fb
krishna221 <krishna.prasath.god@gmail.com>
2013-11-18 12:01:20 +0100

    ts

--- inst/www/JS/scatterplot matrix.js
+++ inst/www/JS/scatterplot matrix.js
@@ -55,7 +55,7 @@ function makeScatterplotMatrix()
             }
         }
         
-        alertPossibleRegressionModel();
+        drawButtonInSideBar("PERFORM MULTIPLE REGRESSION", "regression");
     }
 }
 
