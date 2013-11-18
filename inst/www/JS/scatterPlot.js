104d5c9da44d340052bc225cb1a44f565132c21c
krishna221 <krishna.prasath.god@gmail.com>
2013-11-18 11:52:33 +0100

    this should be fun

--- inst/www/JS/scatterPlot.js
+++ inst/www/JS/scatterPlot.js
@@ -246,40 +246,35 @@ function drawRegressionLine(intercept, slope)
     
     var x1, y1, x2, y2;
     
-    var Y1, Y2;
+    var X1, X2;
     
-    Y1 = 0;
-    Y2 = plotHeight;
-    
-    console.log("uniqueDataX.length = " + uniqueDataX.length);
-    console.log("LEFT=" + LEFT);
-    console.log("Y1=" + Y1);
-    console.log("getValue term=" + getValue1(slope*Y1 + intercept, mins["X"], maxs["X"]));
+    X1 = mins["X"];
+    X2 = maxs["X"];
         
     if(uniqueDataX.length <= numberOfGrooves)
         x1 = LEFT + uniqueDataX.indexOf(slope*Y1 + intercept)*xStep + xStep/2;    
     else
-        x1 = LEFT + getValue1(slope*Y1 + intercept, mins["X"], maxs["X"])*plotWidth;
+        x1 = LEFT + getValue1(X1, mins["X"], maxs["X"])*plotWidth;
         
     if(uniqueDataY.length <= numberOfGrooves)
         y1 = BOTTOM - uniqueDataY.indexOf(Y1)*yStep - yStep/2;
     else
-        y1 = BOTTOM - getValue1(Y1, mins["Y"], maxs["Y"])*plotHeight;
+        y1 = BOTTOM - getValue1(slope*X1 + intercept, mins["Y"], maxs["Y"])*plotHeight;
     
     if(uniqueDataX.length <= numberOfGrooves)
         x2 = LEFT + uniqueDataX.indexOf(slope*Y2 + intercept)*xStep + xStep/2;    
     else
-        x2 = LEFT + getValue1(slope*Y2 + intercept, mins["X"], maxs["X"])*plotWidth;
+        x2 = LEFT + getValue1(X2, mins["X"], maxs["X"])*plotWidth;
         
     if(uniqueDataY.length <= numberOfGrooves)
         y2 = BOTTOM - uniqueDataY.indexOf(Y2)*yStep - yStep/2;
     else
-        y2 = BOTTOM - getValue1(Y2, mins["Y"], maxs["Y"])*plotHeight;
+        y2 = BOTTOM - getValue1(slope*X2 + intercept, mins["Y"], maxs["Y"])*plotHeight;
             
     
     canvas.append("circle")
             .attr("cx", LEFT + getValue1(0, mins["X"], maxs["X"])*plotWidth)
-            .attr("cy", BOTTOM - getValue1((0 - intercept)/slope, mins["Y"], maxs["Y"])*plotHeight)
+            .attr("cy", BOTTOM - getValue1(intercept, mins["Y"], maxs["Y"])*plotHeight)
             .attr("r", "10px")
             .attr("fill", "red")
             .attr("id", "interceptCircle")
