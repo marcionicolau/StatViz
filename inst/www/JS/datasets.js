f82153ba794d708b69b78d2afab92fb2a4565aa0
krishna221 <krishna.prasath.god@gmail.com>
2013-11-19 15:23:50 +0100

    tes

--- inst/www/JS/datasets.js
+++ inst/www/JS/datasets.js
@@ -7,6 +7,7 @@ var datasetInformation = new Object();
     datasetInformation["weightLoss"] = "Dataset comparing the weight lost by participants in 3 groups: those who skipped breakfast, those who skipped lunch, and those who skipped dinner";
     datasetInformation["store"] = "TBD";
     datasetInformation["SAT"] = "When deciding whether to admit an applicant, colleges take lots of factors, such as grades, sports, activities, leadership positions, awards, teacher recommendations, and test scores, into consideration. Using SAT scores as a basis of whether to admit a student or not has created some controversy. Among other things, people question whether the SATs are fair and whether they predict college performance. This study examines the SAT and GPA information of 105 students who graduated from a state university with a B.S. in computer science. Using the grades and test scores from high school, can you predict a student's college grades?";
+    datasetInformation["hotdogs"] = "Results of a laboratory analysis of calories and sodium content of major hot dog brands. Researchers for Consumer Reports analyzed three types of hot dog: beef, poultry, and meat (mostly pork and beef, but up to 15% poultry meat).";
 
 var variablesInDataset = new Object();
     variablesInDataset["impact"] = ["subject","condition","verbalMemoryPre","visualMemoryPre","visualMotorSpeedPre","reactionTimePre","impulseControlPre","totalSymptomPre","verbalMemoryPost","visualMemoryPost","visualMotorSpeedPost","reactionTimePost","impulseConstrolPost","totalSymptomPost"]
@@ -14,6 +15,7 @@ var variablesInDataset = new Object();
     variablesInDataset["weightLoss"] = ["participantID", "ageGroup", "condition", "weightLost"];
     variablesInDataset["store"] = ["ID", "price", "store", "subject"];
     variablesInDataset["SAT"] = ["participantID", "high_GPA", "math_SAT", "verb_SAT", "comp_GPA", "univ_GPA"];
+    variablesInDataset["hotdogs"] = ["Type", "Calories", "Sodium"];
  
 var types = ["participant", "dependent", "independent"];
 var variablesInDatasetType = new Object();
@@ -25,6 +27,7 @@ var variablesInDatasetDataType = new Object();
     variablesInDatasetDataType["weightLoss"] = [dataTypes[0], dataTypes[0], dataTypes[0], dataTypes[3]];
     variablesInDatasetDataType["store"] = [dataTypes[0], dataTypes[3], dataTypes[0], dataTypes[0]];
     variablesInDatasetDataType["SAT"] = [dataTypes[0], dataTypes[2], dataTypes[2], dataTypes[2], dataTypes[2], dataTypes[2]];
+    variablesInDatasetDataType["hotdogs"] = [dataTypes[0], dataTypes[2], dataTypes[2]];
     
 function initVariablesInDatasetTypes()
 {  
@@ -33,4 +36,5 @@ function initVariablesInDatasetTypes()
     variablesInDatasetType["weightLoss"] = [types[0], types[2], types[2], types[1]];
     variablesInDatasetType["store"] = [types[1], types[1], types[2], types[0]];    
     variablesInDatasetType["SAT"] = [types[0], types[1], types[1], types[1], types[1], types[1]];
+    variablesInDatasetType["hotdogs"] = [types[2], types[1], types[1]];
 }
